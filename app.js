fce2b6486783dc3325522d578aea53bb2b02bf3e
v_hayang <v_hayang@tencent.com>
2019-10-21 17:20:38 +0800

    提交-末日生存

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
 //app.js
+var plugin = requirePlugin("myPlugin");
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
+    let height = 0
+    wx.getSystemInfo({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+        console.log(res);
+        let isIOS = res.system.indexOf("iOS") > -1;
+        let navHeight = 0;
+        if (!isIOS) {
+          navHeight = 48;
+        } else {
+          navHeight = 44;
         }
+        height = navHeight + res.statusBarHeight
       }
-    })
+    });
+    plugin.init({
+      appid: "VEgbxLa9kYqzGOzstdeSF3xDbkS9zK",
+      // appid: "P5Ot9PHJDechCYqDFAW1AiK6OtG3Ja",
+      textToSpeech: true,
+      guideList: ["玩末日生存游戏"],
+      // welcome: '请问需要什么帮助',
+      // background: "#eee",
+      guideCardHeight: 50,
+      operateCardHeight: 120,
+      // history: true,
+      // historySize: 60,
+      navHeight: height,
+      success: () => {
+        // plugin.send({
+        //   query: "你好",
+        //   success: res => {
+        //     console.log(res);
+        //   },
+        //   fail: error => {}
+        // });
+      },
+      fail: error => {}
+    });
   },
   globalData: {
     userInfo: null
+  },
+  onHide:function(res) {
+    console.log(res + "-----------------")
   }
 })
